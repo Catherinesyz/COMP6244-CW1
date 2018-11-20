66ba9b6edcd66a7eeb7a5e98c703e8f2465c661c
Catherine <44072572+Catherinesyz@users.noreply.github.com>
2018-11-20 08:46:27 +0000

    Add files via upload

--- admproj.js
+++ admproj.js
@@ -1,21 +1,130 @@
 $(document).ready(function(){
     
+    //Create a project
     $("#create-proj").click(function(){
        
-        $("#cpbtn").submit(function(){
-           
-        });   
+        $("#cpbtn").click(function(){
+            var i = 0;
+            $.get('https://ibcsv2xy6j.execute-api.eu-west-2.amazonaws.com/dev/addproject', 
+            { 
+                "Project": $('#ProjId').val(), 
+                "DueTime": $('#DueTm').val(),
+                "Manager": $('#MangId').val(),
+                "Developer1": $('#DevId1').val(),
+                "Developer2": $('#DevId2').val(),
+                "Developer3": $('#DevId3').val(),
+                "Pstatus": $('#proj-status').val()
+            }, 
+            function(data, status){
+                if (data){
+                    i++;
+                }
+                if(i == 7){
+                    alert("Successfully Create a Project");
+                }
+                
+            });
+            
+            });          
     });
-    $("#create-user").click(function(){
-       
-        $("#cubtn").submit(function(){
-           
-        });   
-    });
+
+    //Search a project
     $("#search-proj").click(function(){
        
-        $("#sbtn").submit(function(){
-           
-        });   
+        $("#sbtn").click(function(){
+            
+            $.get('https://jl53shwlhj.execute-api.eu-west-2.amazonaws.com/dev/searchproj', 
+            { 
+                "Project": $('#sProjId').val()
+            }, 
+            function(data, status){
+                
+                 //   alert("Data: " + data.Item.Project + "\nStatus: " + status);
+                 if(data)
+                 {
+
+                 }
+                
+            });
+            
+        });          
     });
+
+    // show the information about projects
+    $.get('https://3mylc2m0p9.execute-api.eu-west-2.amazonaws.com/dev/projectdetails', 
+    { 
+    }, 
+    function(data) {
+        var t = document.getElementById("showTable");
+
+        for (var i = 0; i < data.Items.length; i++)
+        {
+            var r = document.createElement("tr");
+            
+            var d = document.createElement("td");
+            d.innerHTML = data.Items[i].Project;
+            r.appendChild(d);
+
+            var d = document.createElement("td");
+            d.innerHTML = data.Items[i].DueTime;
+            r.appendChild(d);
+
+            var d = document.createElement("td");
+            d.innerHTML = data.Items[i].Manager;
+            r.appendChild(d);
+
+            var d = document.createElement("td");
+            d.innerHTML = data.Items[i].Developer1;
+            r.appendChild(d);
+
+            var d = document.createElement("td");
+            d.innerHTML = data.Items[i].Developer2;
+            r.appendChild(d);
+
+            var d = document.createElement("td");
+            d.innerHTML = data.Items[i].Developer3;
+            r.appendChild(d);
+
+            var d = document.createElement("td");
+            d.innerHTML = data.Items[i].Pstatus;
+            r.appendChild(d);
+            
+            var a = document.createElement("a");
+            a.innerHTML = "Modify ";
+            a.href="modify.html?project=" + data.Items[i].Project;
+            r.appendChild(a);
+            
+            var a = document.createElement("a");
+            a.innerHTML = " Delete";
+            a.href="";
+            a.id = "dlt";
+            $("#dlt").click(function(){
+                //a.click = function() {
+                    
+                    alert(data.Items[0].Project);
+                    //this.style.display = 'hide';
+                    $.get('https://si68rvio0a.execute-api.eu-west-2.amazonaws.com/dev/deleteproj', 
+                    { 
+                        "Project" : data.Items[0].Project
+                    },
+                       function(data){
+                           if(data) {
+                                alert("Successfully Delete the Project!");
+                           }
+                            else {
+                                alert("Cannot Delete!");
+                            }
+                       } 
+                    )
+                    alert("11");
+                    
+            });
+            //end click
+            r.appendChild(a);
+            
+            t.appendChild(r);
+            
+        }
+    });
+    //end get
 });
--- mngproj.js
+++ mngproj.js
@@ -10,12 +10,12 @@
 
 $(document).ready(function(){
   
-    var i = 0;
+    
     //Create a project
     $("#create-proj").click(function(){
        
         $("#crt-btn").click(function(){
-            
+            var i = 0;
             $.get('https://ibcsv2xy6j.execute-api.eu-west-2.amazonaws.com/dev/addproject', 
             { 
                 "Project": $('#ProjId').val(), 
@@ -31,7 +31,7 @@ $(document).ready(function(){
                     i++;
                 }
                 if(i == 7){
-                    alert("Data: " + data.Items[0].Project + "\nStatus: " + status);
+                    alert("Successfully Create a Project");
                 }
                 
             });
@@ -40,83 +40,81 @@ $(document).ready(function(){
     });
 
     //Search a project
-    $("#search-proj").click(function(){
-       
+    
         $("#sbtn").click(function(){
             
             $.get('https://jl53shwlhj.execute-api.eu-west-2.amazonaws.com/dev/searchproj', 
             { 
-                "Project": $('#sProjId').val()
+                "Project": $('#myInput').val()
             }, 
             function(data, status){
                 
-                 //   alert("Data: " + data.Item.Project + "\nStatus: " + status);
                  if(data)
                  {
-
+                    $(location).attr('href', 'modify.html?project='+data.Item.Project);
+                    
                  }
-                // show the result(project) of search
-                var d1 = document.getElementById("show_proj");
-                d1.setAttribute("class", "col-xs-8 col-sm-6");
 
-                var d2 = document.createElement("div");
-                d2.setAttribute("class", "card");
-
-                var d3 = document.createElement("div");
-                d3.setAttribute("class", "card-body");
-
-                var h4 = document.createElement("h4");
-                h4.setAttribute("class", "card-title");
-
-                var d4 = document.createElement("div");
-                d4.id = "pid";
-                d4.innerHTML = data.Item.Project;
-
-                //var p1 = document.createElement
-                d4.appendChild(d4);
-                d3.appendChild(h4);
-                d2.appendChild(d3);
-                d1.appendChild(d2);
-
-                
             });
             
         });          
-    });
+    
 
     // show the information about projects
     $.get('https://3mylc2m0p9.execute-api.eu-west-2.amazonaws.com/dev/projectdetails', 
     { 
     }, 
     function(data) {
-        alert(data.Items[6].DueTime);
         var t = document.getElementById("showTable");
 
         for (var i = 0; i < data.Items.length; i++)
         {
             var r = document.createElement("tr");
             
-            var d1 = document.createElement("td");
-            d1.innerHTML(data.Items[i].Project);
-            d1.crea
-            r.appendChild(d1);
+            var d = document.createElement("td");
+            d.innerHTML = data.Items[i].Project;
+            r.appendChild(d);
 
-            var d2 = document.createElement("td");
-            d2.innerHTML(data.Items[i].DueTime);
-            r.appendChild(d2);
+            var d = document.createElement("td");
+            d.innerHTML = data.Items[i].DueTime;
+            r.appendChild(d);
 
-            var d3 = document.createElement("td");
-            d3.innerHTML(data.Items[i].Developer1);
-            r.appendChild(d3);
+            var d = document.createElement("td");
+            d.innerHTML = data.Items[i].Manager;
+            r.appendChild(d);
+
+            var d = document.createElement("td");
+            d.innerHTML = data.Items[i].Developer1;
+            r.appendChild(d);
+
+            var d = document.createElement("td");
+            d.innerHTML = data.Items[i].Developer2;
+            r.appendChild(d);
+
+            var d = document.createElement("td");
+            d.innerHTML = data.Items[i].Developer3;
+            r.appendChild(d);
+
+            var d = document.createElement("td");
+            d.innerHTML = data.Items[i].Pstatus;
+            r.appendChild(d);
             
+            var a = document.createElement("a");
+            a.innerHTML = "Modify ";
+            a.href="modify.html?project=" + data.Items[i].Project;
+            r.appendChild(a);
+            
+            
+            //end click
+            r.appendChild(a);
             
             t.appendChild(r);
             
         }
-    }
-    );
-   
-         
+        // end for
+        
+    });
+    // end get     
 });
 
 
